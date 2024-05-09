430bba24cf89685aa10c8d2d79142df11d0b31e7
takielias <taki.elias@gmail.com>
2024-05-09 22:38:21 +0600

    Update js import

--- resources/js/plugins/filepond.js
+++ resources/js/plugins/filepond.js
@@ -15,5 +15,4 @@ window.FilePond.registerPlugin(FilePondPluginImageEdit);
 import '../../../../../../node_modules/filepond/dist/filepond.min.css';
 import '../../../../../../node_modules/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 import '../../../../../../node_modules/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.css';
-import '../../css/tablar-kit.css';
 
